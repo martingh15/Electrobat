6c614e3d5b4c3f3129b6fededce5efe8eaa6cde7
martingh15 <martinghiotti@yahoo.com.ar>
2020-04-13 00:34:54 -0300

    backuo

---
+++ backupv2/precache-manifest.27d85a19cb2770ed954d57a89495ae7f.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f41f4edce9ce98d8343a979e993936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a83b6310662cee9ffc",
+    "url": "/static/css/2.34c9b3d3.chunk.css"
+  },
+  {
+    "revision": "87d5bf34bb856ae8a464",
+    "url": "/static/css/main.af6e1064.chunk.css"
+  },
+  {
+    "revision": "d2a83b6310662cee9ffc",
+    "url": "/static/js/2.819b784e.chunk.js"
+  },
+  {
+    "revision": "87d5bf34bb856ae8a464",
+    "url": "/static/js/main.8027bdf6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a449fc81887342da26dfbf071b298f6",
+    "url": "/static/media/Baterias_Yuasa.8a449fc8.jpg"
+  },
+  {
+    "revision": "8417dfa4818c1fb12b0625ecbdbdaf23",
+    "url": "/static/media/DIN Condensed Bold.8417dfa4.ttf"
+  },
+  {
+    "revision": "2516b34268d5abb314bd31734f2391d0",
+    "url": "/static/media/SourceSansPro-Black.2516b342.otf"
+  },
+  {
+    "revision": "5e1d162e634a102d7da8db597825a764",
+    "url": "/static/media/SourceSansPro-Regular.5e1d162e.otf"
+  },
+  {
+    "revision": "d476f28a365c7eb37704d42b63c93107",
+    "url": "/static/media/TROJAN-LogoL.d476f28a.jpg"
+  },
+  {
+    "revision": "f050997d46d76395ebeaeec301b1ca1f",
+    "url": "/static/media/acdelgo.f050997d.png"
+  },
+  {
+    "revision": "e7c44ab737907a856dced226f6f3fb30",
+    "url": "/static/media/ahora12.e7c44ab7.png"
+  },
+  {
+    "revision": "a54caf841f09d51924f0c5688c49f60e",
+    "url": "/static/media/bateria-12V-100AH-11-toyota-hilux-kia-sorento.a54caf84.png"
+  },
+  {
+    "revision": "4aa8d1dd8ad1336c29ae3ff303b364ad",
+    "url": "/static/media/bateria-12V-100AH-3-camiones-tractores.4aa8d1dd.png"
+  },
+  {
+    "revision": "b17c719cafb406ac40ae4dbcfa0a0e8a",
+    "url": "/static/media/bateria-12V-100AH-camionetas-diesel-modelo-sprinter-4.b17c719c.png"
+  },
+  {
+    "revision": "6062726a4368fefa36d9f6375ee92faf",
+    "url": "/static/media/bateria-12V-110AH-5-camionetas-tractores.6062726a.png"
+  },
+  {
+    "revision": "3cbd8706980a81e47483e30a058c91fb",
+    "url": "/static/media/bateria-12V-180AH-6-camiones-tractores.3cbd8706.png"
+  },
+  {
+    "revision": "f30ffceb24cc09d077329ae24e8d1253",
+    "url": "/static/media/bateria-12V-40AH-9-honda-fit-kia.f30ffceb.png"
+  },
+  {
+    "revision": "a6f74926e140783017bc32eff570aa6c",
+    "url": "/static/media/bateria-12V-45AH-7-ford-k.a6f74926.png"
+  },
+  {
+    "revision": "0c8b1afb5f3ae6182e5dbd1235f63535",
+    "url": "/static/media/bateria-12V-70AH-1-autos-nafteros-gnc.0c8b1afb.png"
+  },
+  {
+    "revision": "2c6a1ffc372c5f075be4606909a9d3c3",
+    "url": "/static/media/bateria-12V-75AH-2-autos-diesel.2c6a1ffc.png"
+  },
+  {
+    "revision": "d08cef5b6b21d74898d7a9c56b9c2dcf",
+    "url": "/static/media/bateria-12V-85AH-10-trafic-toyota-hilux.d08cef5b.png"
+  },
+  {
+    "revision": "a507cfa8b1e046d7bfc9ecfc5b62febd",
+    "url": "/static/media/bateria-12V-85AH-8-modelo-ranger.a507cfa8.png"
+  },
+  {
+    "revision": "429ca9e487f307b2d99fbbce9423b715",
+    "url": "/static/media/bateria-detalle.429ca9e4.jpg"
+  },
+  {
+    "revision": "c4bb497ec1b000766b51a09323d48bb8",
+    "url": "/static/media/baterias.c4bb497e.jpg"
+  },
+  {
+    "revision": "e4e3eae96d5658a018e8c3e2aea3de89",
+    "url": "/static/media/camion.e4e3eae9.jpg"
+  },
+  {
+    "revision": "c34f622153dbf3780c6138a2b97f1901",
+    "url": "/static/media/contacto.c34f6221.jpg"
+  },
+  {
+    "revision": "e4a2cd38591cef1916d55984cf0db210",
+    "url": "/static/media/logo-probattery-400x400.e4a2cd38.png"
+  },
+  {
+    "revision": "f871f1c3d93cafad28a05e5b0cc9f1d8",
+    "url": "/static/media/logoElectrobat.f871f1c3.png"
+  },
+  {
+    "revision": "e0f0ec2fa4bbe07401c29e51bbdedd09",
+    "url": "/static/media/moura.e0f0ec2f.png"
+  },
+  {
+    "revision": "a1ba39c8f0f3b75811637bdcf14f8d08",
+    "url": "/static/media/nosotros.a1ba39c8.jpg"
+  },
+  {
+    "revision": "a9f0de60a75d1f345755a98c4c21867d",
+    "url": "/static/media/slider1.a9f0de60.jpg"
+  },
+  {
+    "revision": "a315dda301dd4ac9533924ceff6fab8f",
+    "url": "/static/media/slider2.a315dda3.jpg"
+  },
+  {
+    "revision": "3806c2506b996574ac0f090c38342491",
+    "url": "/static/media/slider3.3806c250.jpg"
+  },
+  {
+    "revision": "3a989714b7ecdbeaea31006d4e8c0495",
+    "url": "/static/media/slider4.3a989714.jpg"
+  },
+  {
+    "revision": "ed8153e9ef8d41395d0dd6498c0459fd",
+    "url": "/static/media/slider5.ed8153e9.jpg"
+  },
+  {
+    "revision": "65fcd3ec45e9a11bfa39951e2f149726",
+    "url": "/static/media/slider6.65fcd3ec.jpg"
+  },
+  {
+    "revision": "0ddc2b2f5ac224a7890a4b0444bf31d0",
+    "url": "/static/media/volquete.0ddc2b2f.jpg"
+  },
+  {
+    "revision": "a872dbf513cf287c6f1e8715c4c889c4",
+    "url": "/static/media/volquetes.a872dbf5.jpg"
+  },
+  {
+    "revision": "a4f1ea98572a9d9c784e3887d24c1326",
+    "url": "/static/media/whatsapp-verde.a4f1ea98.png"
+  }
+]);
