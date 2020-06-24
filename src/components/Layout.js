1314b55d39571d29d586caa48df0723f2376ab18
martingh15 <martin.ghiotti@innovavista.net>
2020-06-24 20:53:56 -0300

    Deshabilito cartel

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,7 +34,7 @@ class Layout extends Component {
                 <a href={"https://wa.me/5493416868049"} target="_blank"  rel="noopener noreferrer">
                     <img className="whatsapp" src={whatsappVerde} alt="whatsap" title="whatsap"/>
                 </a>
-                <PromoModal/>
+                {/*<PromoModal/>*/}
             </div>
         );
     }
