128f6f709f04e9eb567d7e199239e0095cd88d98
Martín Ghiotti <martin.ghiotti@innovavista.net>
2021-01-24 15:47:08 -0300

    SEO element

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,7 +15,7 @@ y este proyecto adhiere a [Semantic Versioning]( https://semver.org/spec/v2.0.0.
 -
 
 ### Removed
--
+- SEO element.
 
 ### Fixed
 -
--- src/components/elementos/SEO.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useSEO from '../../hooks/useSEO';
-
-export default function SEO(title, description) {
-    useSEO(title, description);
-    return (
-        <div></div>
-    );
-}
